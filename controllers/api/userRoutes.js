70e23137d95a268db5dbe98953ae9616b686f417
jojo <jodiladouceur.design@gmail.com>
2021-06-06 12:44:21 -0400

    edits to public files and routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -59,6 +59,6 @@ router.post('/', async (req, res) => {
 //     }  catch (err) {
 //        res.status(400).json(err)
 //     }
-// });
+// });   
 
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -7,8 +7,7 @@ class User extends Model {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
-
-    User.init(
+     User.init(
         {
             id: { // Users unique Id
                 type: DataTypes.INTEGER,
