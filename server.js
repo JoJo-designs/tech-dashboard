21fd8b9a2edfc1572040fa7c3e883e59c0281f69
jojo <jodiladouceur.design@gmail.com>
2021-06-07 09:55:13 -0400

    edits to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
@@ -16,13 +17,11 @@ const sess = {
   saveUninitialized: false,
 };
 
+
 app.use(session(sess));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
