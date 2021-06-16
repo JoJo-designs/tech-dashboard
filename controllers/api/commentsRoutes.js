93d818ef168cc698d63f765614ce7e61fd7526b6
jojo <jodiladouceur.design@gmail.com>
2021-06-16 13:33:20 -0400

    edits to routes

--- controllers/api/commentsRoutes.js
+++ controllers/api/commentsRoutes.js
@@ -4,24 +4,24 @@ const { Comments, User } = require('../../models')
 // Route /api/comments/
 
 // Route gets all users
-// router.get('/', async (req, res) => {
-//     try {
-//         const commentData = await Comments.findAll({
-//             include: [{ model: User }]
-//         });
-//         res.status(200).json(commentData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comments.findAll({
+            include: [{ model: User }]
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-// router.post('/', async (req, res) => {
-//     try {
-//         const commentData = await Comments.create(req.body);
-//         res.status(200).json(commentData);
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// });
+router.post('/', async (req, res) => {
+    try {
+        const commentData = await Comments.create(req.body);
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
