171a26b25d8b94863f55adfb59c5551551045a44
jojo <jodiladouceur.design@gmail.com>
2021-06-16 13:25:50 -0400

    trying something

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,60 +19,60 @@ router.get('/', async (req,res) => {
 });
 
 // route renders the login page
-// router.get('/login', async (req, res) => {
-//   try {
-//     res.render('login');
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // route renders the sign up page
-// router.get('/signUp', async (req, res) => {
-//   try {
-//     res.render('signUp');
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get('/signUp', async (req, res) => {
+  try {
+    res.render('signUp');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // route renders the dashboard for each user. 
 //  will need an Id and log in data but I will make a filler for now
-// router.get('/dashboard', withAuth, async (req, res) => {
-//   try {
-//     const userUniqueId = req.session.user_id
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userUniqueId = req.session.user_id
 
-//     const userData = await User.findOne({ where: {id: userUniqueId}, 
-//        include: [{ model: Post }] 
-//     });
+    const userData = await User.findOne({ where: {id: userUniqueId}, 
+       include: [{ model: Post }] 
+    });
 
-//     //const userOne = userData.map((user) => user.get({ plain: true }));    
-//     res.render('dashboard', { user: userData.get({ plain: true}) });
-//     //res.render('dashboard', { userOne });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    //const userOne = userData.map((user) => user.get({ plain: true }));    
+    res.render('dashboard', { user: userData.get({ plain: true}) });
+    //res.render('dashboard', { userOne });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // opens the new post handlebars when the user user the new post button
-// router.get('/newpost', withAuth, async (req, res) => {
-//   try {
-//     res.render('newpost');
-//   } catch (err) {
-//     res.status(500).json(err)
-//   }
-// })
+router.get('/newpost', withAuth, async (req, res) => {
+  try {
+    res.render('newpost');
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
-// router.get('/edit/:id', withAuth, async (req, res) => {
-//   try {
-//     let post = await Post.findByPk(req.params.id);
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    let post = await Post.findByPk(req.params.id);
 
-//     post = post.get({ plain: true });
+    post = post.get({ plain: true });
 
-//     res.render('edit', {post});
-//   } catch (err) {
-//     res.status(500).json(err)
-//   }
-// })
+    res.render('edit', {post});
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router;
